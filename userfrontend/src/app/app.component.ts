9148c264570b824c5a298d528d0a0eb846bc85d0
Azizi <mr.krab72@gmail.com>
2023-06-09 08:42:57 +0800

    smol changes

--- userfrontend/src/app/app.component.ts
+++ userfrontend/src/app/app.component.ts
@@ -3,11 +3,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 @Component({
-  selector: 'app-root', // how Angular refers to component in template
-  // standalone: false, -> the component require 'ngModule' or not
-  // imports: [], -> describe component's dependencies (only for standalone)
-  templateUrl: './app.component.html',  // component's HTML markup / layout
-  styleUrls: ['./app.component.css'] // component's CSS
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'userfrontend';
